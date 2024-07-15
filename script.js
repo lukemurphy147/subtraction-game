4d9e54e76820476a0def7a3e51ad0408f8836b15
Luke Murphy <60206183+lukemurphy147@users.noreply.github.com>
2024-07-15 14:13:00 -0400

    Added game logic (two human players)

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
-function validate() {
-    var heapSize = +document.getElementById("heapSize").value;
+function startGame() {
+    var initialHeap = +document.getElementById("initialHeap").value;
     var k = +document.getElementById("k").value;
 
-    if (!Number.isInteger(heapSize) || heapSize < 1 || heapSize > 100) {
-        alert("Invalid heap size");
+    if (!Number.isInteger(initialHeap) || initialHeap < 1) {
+        alert("Invalid initial heap size");
         return;
     }
-    if (!Number.isInteger(k) || k < 1 || k > 10) {
+    if (!Number.isInteger(k) || k < 1 || k > initialHeap) {
         alert("Invalid k");
         return;
     }
 
-    game(heapSize, k);
+    document.getElementById("paramInput").style.display = "none";
+    document.getElementById("game").style.display = "block";
+    
+    document.getElementById("currentHeap").innerHTML = initialHeap;
+    document.getElementById("currentPlayer").innerHTML = "Player 1's";
+    document.getElementById("maxPerMove").innerHTML = k;
 }
 
-function game(heapSize, k) {
-    var divInput = document.getElementById("input");
-    var divGame = document.getElementById("game");
-    divInput.style.display = "none";
-    divGame.style.display = "block";
+function stopGame() {
+    document.getElementById("move").value = "";
+
+    document.getElementById("game").style.display = "none";
+    document.getElementById("paramInput").style.display = "block";
+}
+
+function updateHeap() {
+    var currentPlayer = document.getElementById("currentPlayer").innerHTML;
+    var currentHeap = +document.getElementById("currentHeap").innerHTML;
+    var maxPerMove = +document.getElementById("maxPerMove").innerHTML;
+    var move = +document.getElementById("move").value;
+
+    document.getElementById("move").value = "";
+
+    if (!Number.isInteger(move) || move < 1 || move > maxPerMove) {
+        alert("Invalid move");
+        return;
+    }
+
+    currentHeap -= move;
+    if (currentHeap == 0) {
+        if (currentPlayer == "Player 1's") {
+            alert("Player 1 wins!");
+        }
+        else {
+            alert("Player 2 wins!")
+        }
+        stopGame();
+        return;
+    }
+
+    document.getElementById("currentHeap").innerHTML = currentHeap;
+
+    if (currentHeap < maxPerMove) {
+        maxPerMove = currentHeap;
+        document.getElementById("maxPerMove").innerHTML = maxPerMove;
+    }
+
+    if (currentPlayer == "Player 1's") {
+        document.getElementById("currentPlayer").innerHTML = "Player 2's";
+    }
+    else {
+        document.getElementById("currentPlayer").innerHTML = "Player 1's";
+    }
 }
