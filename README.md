ed5f410f9d4bb4f24b47be32cc251bdb76993200
Luke Murphy <60206183+lukemurphy147@users.noreply.github.com>
2024-07-16 15:50:04 -0400

    Added computer player

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # subtraction-game
-This is a Javascript implementation of [the subtraction game.](https://en.wikipedia.org/wiki/Nim#The_subtraction_game)
+This is a Javascript implementation of [the subtraction game.](https://en.wikipedia.org/wiki/Nim#The_subtraction_game) You can play against the computer (which follows the optimal strategy) or another human (offline only).
 
 You can play it in your browser [here.](https://lukemurphy147.github.io/subtraction-game)
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function startGame() {
     var initialHeap = +document.getElementById("initialHeap").value;
     var k = +document.getElementById("k").value;
@@ -5,8 +7,7 @@ function startGame() {
     if (!Number.isInteger(initialHeap) || initialHeap < 1) {
         alert("Invalid initial heap size");
         return;
-    }
-    if (!Number.isInteger(k) || k < 1 || k > initialHeap) {
+    } else if (!Number.isInteger(k) || k < 1 || k > initialHeap) {
         alert("Invalid k");
         return;
     }
@@ -15,8 +16,19 @@ function startGame() {
     document.getElementById("game").style.display = "block";
     
     document.getElementById("currentHeap").innerHTML = initialHeap;
-    document.getElementById("currentPlayer").innerHTML = "Player 1's";
     document.getElementById("maxPerMove").innerHTML = k;
+
+    var turnOrder = document.getElementById("turnOrder").value;
+    var currentPlayer = document.getElementById("currentPlayer")
+
+    if (turnOrder == "twoHumans") {
+        currentPlayer.innerHTML = "Player 1's";
+    } else if (turnOrder == "humanFirst") {
+        currentPlayer.innerHTML = "your";
+    } else {
+        currentPlayer.innerHTML = "your";
+        computerMove(initialHeap, k);
+    }
 }
 
 function stopGame() {
@@ -26,8 +38,8 @@ function stopGame() {
     document.getElementById("paramInput").style.display = "block";
 }
 
-function updateHeap() {
-    var currentPlayer = document.getElementById("currentPlayer").innerHTML;
+function playerMove() {
+    var currentPlayer = document.getElementById("currentPlayer");
     var currentHeap = +document.getElementById("currentHeap").innerHTML;
     var maxPerMove = +document.getElementById("maxPerMove").innerHTML;
     var move = +document.getElementById("move").value;
@@ -41,11 +53,12 @@ function updateHeap() {
 
     currentHeap -= move;
     if (currentHeap == 0) {
-        if (currentPlayer == "Player 1's") {
+        if (currentPlayer.innerHTML == "Player 1's") {
             alert("Player 1 wins!");
-        }
-        else {
-            alert("Player 2 wins!")
+        } else if (currentPlayer.innerHTML == "Player 2's") {
+            alert("Player 2 wins!");
+        } else {
+            alert("You win! Well played.");
         }
         stopGame();
         return;
@@ -58,10 +71,41 @@ function updateHeap() {
         document.getElementById("maxPerMove").innerHTML = maxPerMove;
     }
 
-    if (currentPlayer == "Player 1's") {
-        document.getElementById("currentPlayer").innerHTML = "Player 2's";
+    if (currentPlayer.innerHTML == "Player 1's") {
+        currentPlayer.innerHTML = "Player 2's";
+    } else if (currentPlayer.innerHTML == "Player 2's") {
+        currentPlayer.innerHTML = "Player 1's";
+    } else {
+        computerMove(currentHeap, maxPerMove);
     }
-    else {
-        document.getElementById("currentPlayer").innerHTML = "Player 1's";
+}
+
+function computerMove(currentHeap, maxPerMove) {
+    if (currentHeap % (maxPerMove + 1) == 0) {
+        /* This is a losing position, so we make a random legal move and hope the player makes a mistake. */
+        move = Math.floor(Math.random() * maxPerMove) + 1;
+    } else {
+        /* This is a winning position, so we put the player in a losing position. */
+        move = currentHeap % (maxPerMove + 1);
+    }
+
+    currentHeap -= move;
+    if (move == 1) {
+        alert(`The computer removes 1 object from the heap.`);
+    } else {
+        alert(`The computer removes ${move} objects from the heap.`);
+    }
+
+    if (currentHeap == 0) {
+        alert("The computer wins! Better luck next time.");
+        stopGame();
+        return;
+    }
+
+    document.getElementById("currentHeap").innerHTML = currentHeap;
+
+    if (currentHeap < maxPerMove) {
+        maxPerMove = currentHeap;
+        document.getElementById("maxPerMove").innerHTML = maxPerMove;
     }
 }
